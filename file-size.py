e069abd3373bd914a3d240590e83370f765204fe
Diana <88678738+dianamiskell@users.noreply.github.com>
2022-07-28 11:23:28 -0400

    Create file-size.py

---
+++ file-size.py
@@ -0,0 +1,5 @@
+# Requirements
+# Walk the file system looking at files, directories, and sub-directories
+# capture file info (file/directory?, size, path)
+# Store info in a suitable data structure
+# report sorted data: largest files
